08946839b40fb2ad5890c0e2de5d09415c677708
Rio Karim <rio.karim@gmail.com>
2019-05-20 16:34:15 +0100

    Assesment Vuex state management

---
+++ src/modules/iec/assessment/index.ts
@@ -0,0 +1,39 @@
+import { createModule } from '@vue-storefront/core/lib/module'
+import axios from 'axios'
+
+// TODO: Move the logic to appropriate modules and deprecate this one
+
+const KEY = 'assessment'
+const store = {
+  namespaced: true,
+  state: {
+    nodeList: [],
+  },
+  mutations: {
+    setAssessment: (state, assessment) => (state.nodeList = assessment)
+  },
+  actions: {
+    async fetchAssessment({ commit }) {
+      const response = await axios.get('http://localhost:3030/assets/iec/assessment.json')
+      commit('setAssessment', response.data)
+    },
+    // async commitAssessment( { commit }, assessment) {
+    //   const response = await axios.post('http://localhost:3030/assets/iec/assessment.json', assessment)
+    //   commit('setAssessment', assessment)
+    // }
+    commitAssessment( { commit }, assessment) {
+      commit('setAssessment', assessment)
+    }
+  },
+  getters: {
+    getAssessment: state => state.nodeList
+  },
+  setters: {
+
+  }
+}
+
+export const Assessment = createModule({
+  key: KEY,
+  store: { modules: [{ key: KEY, module: store }] }
+})
